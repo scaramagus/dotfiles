61bcef007f0b8d7af60e77c47724af0ff3443076
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2018-12-17 20:16:06 -0300

    Update display of git symbols

--- prompt.sh
+++ prompt.sh
@@ -35,6 +35,8 @@ GIT_MOD_LOGO="\uf8ea"
 GIT_EQ_LOGO=""
 GIT_AHEAD_LOGO="\uf04e"
 GIT_BEHIND_LOGO="\uf95e"
+GIT_STASH_LOGO="\ufc24"
+GIT_DIVERGE_LOGO="\ufb40"
 
 _get_virtualenv_prompt() {
     if [ -n "$VIRTUAL_ENV" ]; then
@@ -64,13 +66,15 @@ _get_nodejs_prompt() {
 }
 
 _get_git_prompt() {
-	local GIT_STATUS=$(__git_ps1 "${CYAN_B}$GIT_LOGO %s")
-	GIT_STATUS="${GIT_STATUS/\+/${CYAN} $GIT_ADD_LOGO}"
-	GIT_STATUS="${GIT_STATUS/\=/${CYAN} $GIT_EQ_LOGO}"
-	GIT_STATUS="${GIT_STATUS/\*/${CYAN} $GIT_MOD_LOGO}"
-	GIT_STATUS="${GIT_STATUS/\>/${CYAN} $GIT_AHEAD_LOGO}"
-	GIT_STATUS="${GIT_STATUS/\</${CYAN} $GIT_BEHIND_LOGO}"
-	echo "${GIT_STATUS/\$/}"
+	local GIT_STATUS=$(__git_ps1 "${CYAN}$GIT_LOGO %s")
+	GIT_STATUS="${GIT_STATUS/\+/$GIT_ADD_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\=/$GIT_EQ_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\*/$GIT_MOD_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\<\>/$GIT_DIVERGE_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\>/$GIT_AHEAD_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\</$GIT_BEHIND_LOGO}"
+	GIT_STATUS="${GIT_STATUS/\$/$GIT_STASH_LOGO}"
+	echo $GIT_STATUS
 }
 
 python_interpreter_like_prompt() {
